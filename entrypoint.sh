39d507170c376521c60200acd17951368d40d4dd
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2021-05-30 20:16:47 +0200

    Allow to provide global options (#2)
    Co-authored-by: CrazyMax <crazy-max@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,63 +87,76 @@ fruit:time machine = yes
 
 EOL
 
-for auth in $(yq -j read /data/config.yml | jq -r '.auth[] | @base64'); do
-  _jq() {
-    echo "${auth}" | base64 --decode | jq -r "${1}"
-  }
-  echo "Creating user $(_jq '.user')/$(_jq '.group') ($(_jq '.uid'):$(_jq '.gid'))"
-  id -g "$(_jq '.gid')" &>/dev/null || id -gn "$(_jq '.group')" &>/dev/null || addgroup -g "$(_jq '.gid')" -S "$(_jq '.group')"
-  id -u "$(_jq '.uid')" &>/dev/null || id -un "$(_jq '.user')" &>/dev/null || adduser -u "$(_jq '.uid')" -G "$(_jq '.group')" "$(_jq '.user')" -SHD
-  echo -e "$(_jq '.password')\n$(_jq '.password')" | smbpasswd -a -s "$(_jq '.user')"
-done
+if [[ "$(yq -j read /data/config.yml | jq '.auth')" != "null" ]]; then
+  for auth in $(yq -j read /data/config.yml | jq -r '.auth[] | @base64'); do
+    _jq() {
+      echo "${auth}" | base64 --decode | jq -r "${1}"
+    }
+    echo "Creating user $(_jq '.user')/$(_jq '.group') ($(_jq '.uid'):$(_jq '.gid'))"
+    id -g "$(_jq '.gid')" &>/dev/null || id -gn "$(_jq '.group')" &>/dev/null || addgroup -g "$(_jq '.gid')" -S "$(_jq '.group')"
+    id -u "$(_jq '.uid')" &>/dev/null || id -un "$(_jq '.user')" &>/dev/null || adduser -u "$(_jq '.uid')" -G "$(_jq '.group')" "$(_jq '.user')" -SHD
+    echo -e "$(_jq '.password')\n$(_jq '.password')" | smbpasswd -a -s "$(_jq '.user')"
+  done
+fi
 
-for share in $(yq -j read /data/config.yml | jq -r '.share[] | @base64'); do
-  _jq() {
-    echo "${share}" | base64 --decode | jq -r "${1}"
-  }
-  echo "Creating share $(_jq '.name')"
-  if [[ "$(_jq '.name')" = "null" ]] || [[ -z "$(_jq '.name')" ]]; then
-    >&2 echo "ERROR: Name required"
-    exit 1
-  fi
-  echo -e "\n[$(_jq '.name')]" >> /etc/samba/smb.conf
-  if [[ "$(_jq '.path')" = "null" ]] || [[ -z "$(_jq '.path')" ]]; then
-    >&2 echo "ERROR: Path required"
-    exit 1
-  fi
-  echo "path = $(_jq '.path')" >> /etc/samba/smb.conf
-  if [[ "$(_jq '.comment')" != "null" ]] && [[ -n "$(_jq '.comment')" ]]; then
-    echo "comment = $(_jq '.comment')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.browsable')" = "null" ]] || [[ -z "$(_jq '.browsable')" ]]; then
-    echo "browsable = yes" >> /etc/samba/smb.conf
-  else
-    echo "browsable = $(_jq '.browsable')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.readonly')" = "null" ]] || [[ -z "$(_jq '.readonly')" ]]; then
-    echo "read only = yes" >> /etc/samba/smb.conf
-  else
-    echo "read only = $(_jq '.readonly')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.guestok')" = "null" ]] || [[ -z "$(_jq '.guestok')" ]]; then
-    echo "guest ok = yes" >> /etc/samba/smb.conf
-  else
-    echo "guest ok = $(_jq '.guestok')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.validusers')" != "null" ]] && [[ -n "$(_jq '.validusers')" ]]; then
-    echo "valid users = $(_jq '.validusers')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.adminusers')" != "null" ]] && [[ -n "$(_jq '.adminusers')" ]]; then
-    echo "admin users = $(_jq '.adminusers')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.writelist')" != "null" ]] && [[ -n "$(_jq '.writelist')" ]]; then
-    echo "write list = $(_jq '.writelist')" >> /etc/samba/smb.conf
-  fi
-  if [[ "$(_jq '.veto')" != "null" ]] && [[ "$(_jq '.veto')" = "no" ]]; then
-    echo "veto files = /._*/.apdisk/.AppleDouble/.DS_Store/.TemporaryItems/.Trashes/desktop.ini/ehthumbs.db/Network Trash Folder/Temporary Items/Thumbs.db/" >> /etc/samba/smb.conf
-    echo "delete veto files = yes" >> /etc/samba/smb.conf
-  fi
-done
+if [[ "$(yq -j read /data/config.yml | jq '.global')" != "null" ]]; then
+  for global in $(yq -j read /data/config.yml | jq -r '.global[] | @base64'); do
+  echo "Add global option: $(echo "$global" | base64 --decode)"
+  cat >> /etc/samba/smb.conf <<EOL
+$(echo "$global" | base64 --decode)
+EOL
+  done
+fi
+
+if [[ "$(yq -j read /data/config.yml | jq '.share')" != "null" ]]; then
+  for share in $(yq -j read /data/config.yml | jq -r '.share[] | @base64'); do
+    _jq() {
+      echo "${share}" | base64 --decode | jq -r "${1}"
+    }
+    echo "Creating share $(_jq '.name')"
+    if [[ "$(_jq '.name')" = "null" ]] || [[ -z "$(_jq '.name')" ]]; then
+      >&2 echo "ERROR: Name required"
+      exit 1
+    fi
+    echo -e "\n[$(_jq '.name')]" >> /etc/samba/smb.conf
+    if [[ "$(_jq '.path')" = "null" ]] || [[ -z "$(_jq '.path')" ]]; then
+      >&2 echo "ERROR: Path required"
+      exit 1
+    fi
+    echo "path = $(_jq '.path')" >> /etc/samba/smb.conf
+    if [[ "$(_jq '.comment')" != "null" ]] && [[ -n "$(_jq '.comment')" ]]; then
+      echo "comment = $(_jq '.comment')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.browsable')" = "null" ]] || [[ -z "$(_jq '.browsable')" ]]; then
+      echo "browsable = yes" >> /etc/samba/smb.conf
+    else
+      echo "browsable = $(_jq '.browsable')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.readonly')" = "null" ]] || [[ -z "$(_jq '.readonly')" ]]; then
+      echo "read only = yes" >> /etc/samba/smb.conf
+    else
+      echo "read only = $(_jq '.readonly')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.guestok')" = "null" ]] || [[ -z "$(_jq '.guestok')" ]]; then
+      echo "guest ok = yes" >> /etc/samba/smb.conf
+    else
+      echo "guest ok = $(_jq '.guestok')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.validusers')" != "null" ]] && [[ -n "$(_jq '.validusers')" ]]; then
+      echo "valid users = $(_jq '.validusers')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.adminusers')" != "null" ]] && [[ -n "$(_jq '.adminusers')" ]]; then
+      echo "admin users = $(_jq '.adminusers')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.writelist')" != "null" ]] && [[ -n "$(_jq '.writelist')" ]]; then
+      echo "write list = $(_jq '.writelist')" >> /etc/samba/smb.conf
+    fi
+    if [[ "$(_jq '.veto')" != "null" ]] && [[ "$(_jq '.veto')" = "no" ]]; then
+      echo "veto files = /._*/.apdisk/.AppleDouble/.DS_Store/.TemporaryItems/.Trashes/desktop.ini/ehthumbs.db/Network Trash Folder/Temporary Items/Thumbs.db/" >> /etc/samba/smb.conf
+      echo "delete veto files = yes" >> /etc/samba/smb.conf
+    fi
+  done
+fi
 
 testparm -s
 
